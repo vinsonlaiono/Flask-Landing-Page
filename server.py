0457cc538dafdbd77ba0541748e1cc12e5966973
vinson aiono <vinson.aiono@gmail.com>
2018-04-06 08:52:11 -0700

    added a form to route to another page with input data

--- server.py
+++ server.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/ninjas')
-def ninjas():
-    return render_template('ninjas.html')
+# @app.route('/ninjas')
+# def ninjas():
+#     return render_template('ninjas.html')
 
-@app.route('/dojo')
-def dojo():
-    return render_template('dojo.html')
+# @app.route('/dojo')
+# def dojo():
+#     return render_template('dojo.html')
 
+@app.route('/process', methods=['POST'])
+def process():
+    print('Welcome Ninja')
+    name = request.form['name']
+    return render_template('ninjas.html', name = name)
 if __name__=="__main__":
     app.run(debug = True)
